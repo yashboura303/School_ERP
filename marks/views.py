93c30f087fad2c715d44f7585b48fdd3b503a903
yash <yashboura303@gmail.com>
2019-12-12 17:27:47 +0530

    add exam,subject and marks to students

--- marks/views.py
+++ marks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import ExamType, SubjectsToClass
+from .models import ExamType, SubjectsToClass, Marks
 from classform.models import ClassRoom, ClassRoomStudent
 from django.http import HttpResponse
 from django.contrib import messages
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.http import JsonResponse
 # Create your views here.
 def home(request):
-    return render(request, 'marks/home.html')
+    return render(request, 'marks/home.html',{"class_rooms":ClassRoom.objects.all()})
 
 
 def add_exam_form(request):
@@ -41,12 +41,39 @@ def add_subject_form(request):
         return HttpResponse('')
 
 def add_marks(request):
+
+    exam_types = ExamType.objects.all()
+    class_rooms = ClassRoom.objects.all()
+
     if request.method == "GET":
-        class_name = request.GET["class_name"]
-        exam_type = request.GET["exam_type"]
-        subject = request.GET["subject"]   
+        if "class_name" in request.GET:
+            class_name = request.GET["class_name"]
+            request.session["class_name"] = class_name
+        if "exam_type" in request.GET:
+            exam_type = request.GET["exam_type"]
+            request.session["exam_type"] = exam_type
+        if "subject" in request.GET:
+            subject = request.GET["subject"]   
+            request.session["subject"] = subject     
+            try:
+                students = ClassRoomStudent.objects.filter(classRoom__classSection__icontains = class_name)
+                return render(request, 'marks/addMarks.html',{"students":students,"values":request.GET, "exam_types":exam_types,"class_rooms":class_rooms})
+            except:
+                messages.error(request, "Class doesn't exist")
+                redirect('addMarks')
 
+    if request.method == "POST":
+        try:
+            classstudents = ClassRoomStudent.objects.filter(
+                    classRoom__classSection__exact=request.session["class_name"])
+            exam_type = ExamType.objects.get(examType=request.session["exam_type"])
+            for classroomstudent in classstudents:
+                student_marks = Marks.objects.create(classroomStudent=classroomstudent, 
+                    examType= exam_type,
+                    marks=request.POST[str(classroomstudent.student.admissionNumber)],
+                    subject= request.session["subject"])
+        except:
+            messages.error(request, "Please enter correct info")
+            redirect('addMarks')
 
-        students = ClassRoomStudent.objects.filter(classRoom__classSection__icontains = class_name)
-        return render(request, 'marks/addMarks.html',{"students":students,"values":request.GET})
-    return render(request, 'marks/addMarks.html')
+    return render(request, 'marks/addMarks.html',{"exam_types":exam_types,"class_rooms":class_rooms})
