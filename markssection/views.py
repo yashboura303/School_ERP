cd2677109402c3fb5fec8470546aa77bfd3ef0e9
yash <yashboura303@gmail.com>
2019-12-17 12:42:37 +0530

    added avg/min/max marks graph in markssection

--- markssection/views.py
+++ markssection/views.py
@@ -13,6 +13,9 @@ matplotlib.use('TkAgg')
 
 
 def home(request):
+    """
+    Main page for marks section
+    """
     context = {
 
         "class_rooms": ClassRoom.objects.all(),
@@ -23,6 +26,10 @@ def home(request):
 
 
 def add_exam_form(request):
+    """
+    Add exams for eg. UT-1, SA-1.
+    Input: Exam Name and Description
+    """
     if request.method == "POST":
         if "exam_name" in request.POST:
             exam_name = request.POST["exam_name"]
@@ -35,6 +42,10 @@ def add_exam_form(request):
 
 
 def exam_type_form(request):
+    """
+    Add Exam Type like Oral , Practical, Theory
+    Input: Exam Name, Exam Type Name, Min/Max Marks, Priority
+    """
     if request.method == "POST":
         if "min_marks" in request.POST:
             min_marks = request.POST["min_marks"]
@@ -54,6 +65,10 @@ def exam_type_form(request):
 
 
 def exam_mapping(request):
+    """
+    Map exam type with Exam Name eg. SA-1 mapped with Theory
+    Input: Subject, ClassRoom, Exam Name & Type
+    """
     if request.method == "POST":
         if "class_subject" in request.POST:
             class_subject = request.POST["class_subject"]
@@ -73,29 +88,12 @@ def exam_mapping(request):
         return HttpResponse('')
 
 
-def add_subject_form(request):
-    if request.method == "POST":
-        if "class_room" in request.POST:
-            class_room = request.POST["class_room"]
-        if "class_subject" in request.POST:
-            class_subject = request.POST["class_subject"]
-
-        _class = ClassRoom.objects.get(classSection__exact=class_room)
-
-        if SubjectsToClass.objects.filter(classroom__exact=_class):
-            s = SubjectsToClass.objects.get(classroom=_class)
-            s.subjects += ","+class_subject
-            s.save()
-            return HttpResponse('')
-
-        SubjectsToClass.objects.create(
-            classroom=_class, subjects=class_subject)
-
-        return HttpResponse('')
-
-
 def add_marks(request):
-
+    """
+    Register marks for students 
+    Input: ClassName, Exam Type and Exam Name, Subject
+    Output: A list of students in a classroom and multiple input field for entering marks
+    """
     exam_types = ExamType.objects.all()
     exam_names = Exam.objects.all()
     class_rooms = ClassRoom.objects.all()
@@ -149,6 +147,10 @@ def add_marks(request):
 
 
 def additional_sub_mapping(request):
+    """
+    Add additional subject for each student "
+    Input: Admission Number, Exam Name and Type, Subject Name, Marks
+    """
     if request.method == "POST":
         print(request.POST)
         if "add_number" in request.POST:
@@ -182,6 +184,11 @@ def report_card(request, pk):
 
 
 def report_analysis(request):
+    """
+    Generate graph for SA's and UT's for a given student
+    Input: Student's Admission Number
+    Output: Bar graph for a given term or combined term with all UT's and SA's
+    """
     if request.method == 'GET':
         plt.style.use("fivethirtyeight")
         if "add_number" in request.GET:
@@ -193,13 +200,15 @@ def report_analysis(request):
             class_section = class_student.classRoom.classSection
             class_room = ClassRoom.objects.get(classSection=class_section)
             subject_x = []
+
+            # get subjects list for a particular classroom
             exam_mapping = ExamMapping.objects.filter(classroom=class_room)
             for a in exam_mapping:
                 if a.subject not in subject_x:
                     subject_x.append(a.subject)
-
             subject_x.sort()
-            print("subjects", subject_x)
+
+            # Get UT and SA marks according to term 1 or term 2
             try:
                 if 'term1' in term and 'term2' not in term:
                     marks = Marks.objects.filter(
@@ -312,6 +321,11 @@ def report_analysis(request):
 
 
 def class_report_analysis(request):
+    """
+    Generate two graphs one for pass/fail and other for min/max/avg in classroom.
+    Input: Class/Section
+    Output: Two graph, pass/fail pie chart & bar graph of min/max/avg
+    """
     if request.method == "GET":
         plt.style.use("fivethirtyeight")
         if "class_room" in request.GET:
@@ -338,32 +352,56 @@ def class_report_analysis(request):
             plt.tight_layout()
             plt.show()
 
-
-
+            # max, min and abg bar graph
             class_room = ClassRoom.objects.get(classSection=class_room)
             subject_x = []
             exam_mapping = ExamMapping.objects.filter(classroom=class_room)
+
+            # get subjects
             for a in exam_mapping:
                 if a.subject not in subject_x:
                     subject_x.append(a.subject)
-
             subject_x.sort()
+            # get min,max,avg for each subject and plot bar graph
+            highest = []
+            minimum = []
+            average = []
+            marks = marks.filter(
+                examName__examName='SA-1') | marks.filter(examName__examName='SA-2')
 
             for sub in subject_x:
+                _marks = marks.filter(subject=sub)
+                highest_marks = 0
+                total_marks = 0
+                min_marks = 999
+                count = len(_marks)
+                for mark in _marks:
+                    # count += 1
+                    total_marks += mark.marks
+                    if mark.marks > highest_marks:
+                        highest_marks = mark.marks
+                    if mark.marks < min_marks:
+                        min_marks = mark.marks
 
-            highest_marks = 0
-            total_marks = 0
-            avg_marks = 0
+                average.append(total_marks/count)
+                highest.append(highest_marks)
+                minimum.append(min_marks)
 
-            for mark in marks:
-                total_marks+=mark.marks
-                if mark.marks > highest_marks:
-                    highest_marks = mark.marks
-
-
-            bar(subject, highest_marks)
-            bar(subject, min_marks)
-            bar(subject, avg_marks)
+            width = 0.22
+            subjects = subject_x
+            subject_x = np.arange(len(subject_x))
+            plt.xticks(subject_x, subjects)
+            plt.bar(subject_x - width, highest, color="#444444",
+                    label="Max Marks", width=width, align='center')
+            plt.bar(subject_x, average, color="#008fd5",
+                    label="Avg Marks", width=width, align='center')
+            plt.bar(subject_x + width, minimum, color="#4d6c67",
+                    label="Min Marks", width=width, align='center')
+            plt.legend()
+            plt.title("Max, Avg and Min Standing")
+            plt.xlabel("Subjects")
+            plt.ylabel("Marks")
+            plt.show()
             redirect('reportClass')
 
     return render(request, 'marks/classReport.html', {"class_rooms": ClassRoom.objects.all()})
