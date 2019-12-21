1d5e1a6844e3c0df06ce1d5e77215bc3051af813
jdmrischool <jdmrischool@gmail.com>
2019-12-21 18:44:31 +0530

    system requirements to setup project

---
+++ startup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt install python3-pip
+sudo apt install -y python3-venv
+sudo apt install git
+sudo apt install postgresql postgresql-contrib
