42a0712b0781c1d1db90b8b5a3cfd4d9ed4c1a3c
yash <yashboura303@gmail.com>
2020-01-24 02:13:23 +0530

    added gallery session

--- gallery/models.py
+++ gallery/models.py
@@ -6,18 +6,18 @@ def album_directory_path(instance, filename):
     return 'gallery/album/{0}/{1}'.format(instance.album_name, filename)
 class Album(models.Model):
 	album_name = models.CharField(max_length=30)
-	models.FileField(upload_to=album_directory_path, max_length=100, attributes)
+	file = models.FileField(upload_to=album_directory_path)
 
 def photo_directory_path(instance, filename):
     """file will be uploaded to given path"""
     return 'gallery/photos/{0}/{1}'.format(instance.photo_name, filename)
 class Photo(models.Model):
 	photo_name = models.CharField(max_length=30)
-	models.FileField(upload_to=album_directory_path, max_length=100, attributes)
+	file = models.FileField(upload_to=photo_directory_path)
 
 def video_directory_path(instance, filename):
     """file will be uploaded to given path"""
-    return 'gallery/video/{0}/{1}'.format(instance.photo_name, filename)
+    return 'gallery/video/{0}/{1}'.format(instance.video_name, filename)
 class Video(models.Model):
 	video_name = models.CharField(max_length=30)
-	models.FileField(upload_to=album_directory_path, max_length=100, attributes)
+	file = models.FileField(upload_to=video_directory_path)
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Album, Photo, Video
 # Create your views here.
 def add_album(request):
-	return HttpResponse("hi")
+	if request.method == "POST":
+		album = request.FILES.get("album")
+		album_name = request.POST.get("album_name")
+		Album.objects.create(album_name=album_name, file=album)
+	return render(request, 'gallery/album.html')
+
+def add_video(request):
+	if request.method == "POST":
+		video = request.FILES.get("video")
+		video_name = request.POST.get("video_name")
+		Video.objects.create(video_name=video_name, file=video)
+	return render(request, 'gallery/video.html')
+
+def add_photo(request):
+	if request.method == "POST":
+		photo = request.FILES.get("photo")
+		photo_name = request.POST.get("photo_name")
+		Photo.objects.create(photo_name=photo_name, file=photo)
+	return render(request, 'gallery/photo.html')
